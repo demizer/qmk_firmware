14280e157d8cea3a45851881a5e54121df8eb589
Jesus Alvarez <jeezusjr@gmail.com>
2023-06-26 17:45:39 -0700

    Last unsaved change

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/demizer/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/demizer/keymap.c
@@ -79,10 +79,10 @@ void supere_reset(qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE] = LAYOUT_5x6(
-        _______, _______    , _______       , _______       , _______       , _______   ,           _______     , _______       , _______       , _______   , _______   , _______   ,
-        KC_LGUI, KC_QUOTE   , TD(COMMAQ)    , TD(DOTXLM)    , KC_P          , KC_Y      ,           KC_F        , KC_G          , KC_C          , KC_R      , KC_L      , KC_CAPS   ,
+        KC_LGUI, _______    , KC_7          , KC_0          , KC_1          , KC_6      ,           KC_5        , KC_2          , KC_3          , KC_8      , KC_9      , KC_4      ,
+        KC_TAB , KC_QUOTE   , TD(COMMAQ)    , TD(DOTXLM)    , KC_P          , KC_Y      ,           KC_F        , KC_G          , KC_C          , KC_R      , KC_L      , KC_CAPS   ,
         KC_LCTL, KC_A       , KC_O          , KC_E          , KC_U          , KC_I      ,           KC_D        , KC_H          , KC_T          , KC_N      , KC_S      , KC_RCTL   ,
-        KC_ESC , KC_SCLN    , KC_Q          , KC_J          , KC_K          , KC_X      ,           KC_B        , KC_M          , KC_W          , KC_V      , KC_Z      , KC_SWIN   ,
+        KC_ESC, KC_SCLN    , KC_Q          , KC_J          , KC_K          , KC_X      ,           KC_B        , KC_M          , KC_W          , KC_V      , KC_Z      , KC_SWIN   ,
                               _______       , _______       ,                                                                     _______       , _______   ,
                                               LA_NAV        , KC_BACKSPACE  ,                                     KC_SPC        , LA_SYM        ,
                                               KC_LSFT       , LA_MEG        ,                                     KC_ENTER      , KC_RSFT       ,
